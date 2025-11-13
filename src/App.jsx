17441e0efbedc811612e0ebfd0dc413b1a68a18f
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-13 18:53:22 +0100

    installed Axios and added API endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+const endpointUrl = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
 
 function App() {
   const [formData, setFormData] = useState({
